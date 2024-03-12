c6936abddab4839b92c058df2f543f9a1a0e39f0
NguyenHuuThang10 <nguyenhuuthangag123@gmail.com>
2024-03-12 12:41:36 +0700

    Shoes

--- src/app/controllers/MeController.js
+++ src/app/controllers/MeController.js
@@ -34,7 +34,7 @@ class MeController {
     try {
       req.body.image = req.file.filename;
       await Shoe.createShoe(req.body);
-      res.redirect("/me/stored/shoes");
+      res.redirect("back"); ///me/stored/shoes
     } catch (error) {
       next(error);
     }
