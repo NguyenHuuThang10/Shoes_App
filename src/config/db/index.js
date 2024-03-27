00fbaf33bcd62605cf9066741041302b995e8697
NguyenHuuThang10 <nguyenhuuthangag123@gmail.com>
2024-03-27 21:31:32 +0700

    Remove Nodemon

--- src/config/db/index.js
+++ src/config/db/index.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose')
 
 async function connect() {
     try {
-        await mongoose.connect('mongodb://127.0.0.1:27017/Shoes_app');
+        await mongoose.connect('mongodb+srv://root:123@cluster0.watqp3z.mongodb.net/Shoes_app?retryWrites=true&w=majority&appName=Cluster0');
         console.log('Connect successfully !')
     } catch (error) {
         console.log('Connect fail !')
