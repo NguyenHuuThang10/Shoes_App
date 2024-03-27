813a7813ed8092b14f8e9d10ce440680e47bfc92
NguyenHuuThang10 <nguyenhuuthangag123@gmail.com>
2024-03-28 00:37:34 +0700

    br

--- src/app/controllers/SiteController.js
+++ src/app/controllers/SiteController.js
@@ -274,7 +274,7 @@ class SiteController {
         var sendMail = mailer.sendMail(
           checkEmail.email,
           "RESET PASSWORD",
-          `Hi, ${checkEmail.name}. Vui lòng bấm vào nút "Reset" để được đặt lại mật khẩu.  <a style="padding: 8px 16px; background-color: green; color: #fff; text-decoration: none;" href="${process.env.APP_URL}/reset?email=${checkEmail.email}&token=${token}"> Reset </a>`
+          `Hi, ${checkEmail.name}. Vui lòng bấm vào nút "Reset" để được đặt lại mật khẩu. <br> <a style="padding: 8px 16px; background-color: green; color: #fff; text-decoration: none;" href="${process.env.APP_URL}/reset?email=${checkEmail.email}&token=${token}"> Reset </a>`
         );
         if (sendMail) {
           res.render("form/forgotPassword", {
